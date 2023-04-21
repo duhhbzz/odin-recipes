797b55579cf61020164751e966d6cf9ced35142e
duhhbzz <77603188+duhhbzz@users.noreply.github.com>
2023-04-21 01:30:45 -0500

    added flyout menu functionality to nav element on each page, the odin logo pops out the nav menu

---
+++ resources/main.js
@@ -0,0 +1,12 @@
+const menuButton = document.getElementById("menu-btd");
+const navigation = document.querySelector("nav");
+const closeButton = document.getElementById("close-menu");
+
+menuButton.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+});
+
+closeButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  navigation.classList.remove("active");
+});
